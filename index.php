4a04184480d415279888f3e12adf870fbb09b2c8
francisco <kiko.fccarvalho@gmail.com>
2018-01-09 18:06:14 -0200

    Criado e configurado o autoload

--- index.php
+++ index.php
@@ -4,11 +4,17 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 
-$app->config('debug', true);
+echo $app->config('debug', true);
+
 
 $app->get('/', function() {
     
-	echo "OK";
+  $sql = new \Code\DB\Sql();
+ 
+    $results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
+	 
 
 });
 
