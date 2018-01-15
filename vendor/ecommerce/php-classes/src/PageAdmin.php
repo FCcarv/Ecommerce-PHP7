7ea5c5bab03dbfd959fe8c642d74388007aa29c7
francisco <kiko.fccarvalho@gmail.com>
2018-01-15 14:18:21 -0200

    adicionar arquivos anteriormente ignorados

---
+++ vendor/ecommerce/php-classes/src/PageAdmin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Code;
+
+class PageAdmin extends Page{
+	public function __construct($opts = array(), $tpl_dir = "/GitRepository/Ecommerce-PHP7/views/admin/")
+	{
+		parent :: __construct($opts, $tpl_dir);
+
+	}
+}
